200dfd287b0695d809f5e863af67f60f57ffbb1d
Markus Barenhoff <mbarenh@alios.org>
2011-01-27 12:21:21 +0100

    cleaned up Main

--- Main.hs
+++ Main.hs
@@ -12,34 +12,6 @@ import Network.OAuth.Http.Browser
 
 
 fnordspaceOAuthAPIKey = "mJKuzrfsLKrccB4SuHRnfw"
---app = 
-
-
-accUrl    = fromJust . parseURL $ "https://api.twitter.com/oauth/authorize"
-srvUrl    = fromJust . parseURL $ "https://service/path/to/resource/"
-
-
-app       = Application "consumerKey" "consumerSec" OOB
-
-
-
-reqUrl    = fromJust . parseURL $ "https://api.twitter.com/oauth/request_token"
-reqAuth   = signRq2 PLAINTEXT Nothing reqUrl >>= oauthRequest Browser
-
---foo = reqAuth >>= cliAskAuthorization authUrl
-
-{-
-response  = runOAuthM (fromApplication app) $ 
-                 do { signRq2 PLAINTEXT Nothing reqUrl >>= oauthRequest Browser
-                    ; cliAskAuthorization authUrl
-                    ; signRq2 PLAINTEXT Nothing accUrl >>= oauthRequest Browser
-                    ; signRq2 HMACSHA1 (Just $ Realm "realm") srvUrl >>= serviceRequest Browser
-                    }
--}
-
-
-
-
 
 main :: IO Int
 main = do return 0
